cd72d4acc5c583e3ea22042642e506485d0fa8c1
Arnold W <awang@ingeniux.com>
2016-12-20 12:00:47 -0800

    Update grid.d.ts

--- Grid.d.ts
+++ Grid.d.ts
@@ -4,11 +4,26 @@ export { Cell } from "./src/components/Cell";
 export { HeaderColumn } from "./src/components/HeaderColumn";
 export { Page } from "./src/components/Page";
 export { Row } from "./src/components/Row";
+export { ColumnResizer } from "./src/directives/ColumnResizer";
+export { GridDragSource } from "./src/directives/GridDragSource";
+export { GridDropTarget } from "./src/directives/GridDropTarget";
 export { LiveScroll } from "./src/directives/LiveScroll";
+export { ResizerPlaceHolder } from "./src/directives/ResizePlaceHolder";
 export { WidthUnitType } from "./src/models/enums";
-export { GridCell, GridColumnDef, GridDataRequest, GridDataResponse, GridRow, SelectItemsState, SelectRangeState, SortState } from "./src/models/GridModels";
+export {
+	GridCell, GridColumnDef, GridDataRequest, GridDataResponse,
+	GridRow, SelectItemsState, SelectRangeState, SortState
+} from "./src/models/GridModels";
+export { BoxCoords, Coords, DragSourceModel, DropTargetModel, Gravity } from "./src/models/DndModels";
 export { AsyncPipeService } from "./src/services/AsyncPipeService";
 export { GridDataServiceBase } from "./src/services/GridDataService";
 export { ReactiveGridPageService, ReactiveGridService } from "./src/services/GridReactiveServices";
 export { SelectService } from "./src/services/SelectService";
 export { SortingService } from "./src/services/SortingService";
+export { DndService } from './src/services/DndService';
+export { ColumnResizeService } from './src/services/ColumnResizeService';
+
+
+
+
+
