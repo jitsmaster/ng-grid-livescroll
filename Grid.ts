7081a1d673be86504b86481c47c0dfa16edcbf2c
Arnold W <awang@ingeniux.com>
2016-12-20 11:20:30 -0800

    Fix grid.d.ts not including all types issue

--- Grid.ts
+++ Grid.ts
@@ -1,18 +1,27 @@
-export {Ng2GridModule} from "./src/ng2grid.module";
-export {AwGrid} from "./src/components/AwGrid";
-export {Cell} from "./src/components/Cell";
-export {HeaderColumn} from "./src/components/HeaderColumn";
-export {Page} from "./src/components/Page";
-export {Row} from "./src/components/Row";
-export {LiveScroll} from "./src/directives/LiveScroll";
-export {WidthUnitType} from "./src/models/enums";
-export {GridCell, GridColumnDef, GridDataRequest, GridDataResponse,
-	GridRow, SelectItemsState, SelectRangeState, SortState} from "./src/models/GridModels";
-export {AsyncPipeService} from "./src/services/AsyncPipeService";
-export {GridDataServiceBase} from "./src/services/GridDataService";
-export {ReactiveGridPageService, ReactiveGridService} from "./src/services/GridReactiveServices";
-export {SelectService} from "./src/services/SelectService";
-export {SortingService} from "./src/services/SortingService";
+export { Ng2GridModule } from "./src/ng2grid.module";
+export { AwGrid } from "./src/components/AwGrid";
+export { Cell } from "./src/components/Cell";
+export { HeaderColumn } from "./src/components/HeaderColumn";
+export { Page } from "./src/components/Page";
+export { Row } from "./src/components/Row";
+export { ColumnResizer } from "./src/directives/ColumnResizer";
+export { GridDragSource } from "./src/directives/GridDragSource";
+export { GridDropTarget } from "./src/directives/GridDropTarget";
+export { LiveScroll } from "./src/directives/LiveScroll";
+export { ResizerPlaceHolder } from "./src/directives/ResizePlaceHolder";
+export { WidthUnitType } from "./src/models/enums";
+export {
+	GridCell, GridColumnDef, GridDataRequest, GridDataResponse,
+	GridRow, SelectItemsState, SelectRangeState, SortState
+} from "./src/models/GridModels";
+export { BoxCoords, Coords, DragSourceModel, DropTargetModel, Gravity } from "./src/models/DndModels";
+export { AsyncPipeService } from "./src/services/AsyncPipeService";
+export { GridDataServiceBase } from "./src/services/GridDataService";
+export { ReactiveGridPageService, ReactiveGridService } from "./src/services/GridReactiveServices";
+export { SelectService } from "./src/services/SelectService";
+export { SortingService } from "./src/services/SortingService";
+export { DndService } from './src/services/DndService';
+export { ColumnResizeService } from './src/services/ColumnResizeService';
 
 
 
